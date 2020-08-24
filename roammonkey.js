001a1755e49cc3bb9ab8291e6efaa3bbf4bbd0e8
Erik Newhard <64292121+everruler12@users.noreply.github.com>
2020-08-24 12:02:50 -0600

    Update roammonkey.js

--- roammonkey.js
+++ roammonkey.js
@@ -1,7 +1,13 @@
-const package_list = window.roammonkey_package_list.trim.split('\n')
+const package_list = window.roammonkey_package_list.trim().split('\n')
 
-package_list.forEach(loadPackage)
+const packages = package_list.forEach(loadPackage)
 
 function loadPackage(url) {
-
+  console.log(url)
+  fetch(url)
+    .then(res => res.json())
+    .then((out) => {
+      console.log(out)
+    })
+    .catch(err => { throw err })
 }
