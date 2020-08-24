042f826a49bfbf879f9421e77ce390ae716f02e1
Erik Newhard <64292121+everruler12@users.noreply.github.com>
2020-08-24 15:09:22 -0600

    Update roammonkey.js

--- roammonkey.js
+++ roammonkey.js
@@ -1,13 +1,20 @@
+console.log('RoamMonkey: loaded')
+
+if(!window.jQuery) {
+  var s = document.createElement('script')
+  s.type = "text/javascript"
+  s.src = "https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"
+  s.async = false
+  document.getElementsByTagName('head')[0].appendChild(s)
+}
+
 const package_list = window.roammonkey_package_list.trim().split('\n')
 
-const packages = package_list.forEach(loadPackage)
+const packages = package_list.forEach(loadPackage.packages)
 
 function loadPackage(url) {
-  console.log(url)
-  fetch(url)
-    .then(res => res.json())
-    .then((out) => {
-      console.log(out)
-    })
-    .catch(err => { throw err })
+  console.log("RoamMonkey: Getting ", url)
+  $.getScript(url, function(data){
+      console.log("RoamMonkey: Loaded ", url)
+  })
 }
