9d090a23b5478969e54eab9c990ad6bb19d80f37
Erik Newhard <64292121+everruler12@users.noreply.github.com>
2020-08-24 18:34:00 -0600

    Update roammonkey.js

--- roammonkey.js
+++ roammonkey.js
@@ -2,7 +2,7 @@ console.log('RoamMonkey: loaded')
 
 // if(!window.jQuery)
 roamMonkey_include("https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js")
-roamMonkey_include("https://cdnjs.cloudflare.com/ajax/libs/vue/2.6.12/vue.min.js")
+roamMonkey_include("https://cdn.jsdelivr.net/npm/vue/dist/vue.js")
 roamMonkey_include("https://cdnjs.cloudflare.com/ajax/libs/axios/0.20.0/axios.min.js")
 
 // remove duplicate button
