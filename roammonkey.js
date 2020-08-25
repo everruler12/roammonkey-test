a731cec0975f511aa08d573bd9d899a8e72d9050
Erik Newhard <64292121+everruler12@users.noreply.github.com>
2020-08-24 19:53:09 -0600

    Update roammonkey.js

--- roammonkey.js
+++ roammonkey.js
@@ -1,10 +1,45 @@
 console.log('RoamMonkey: loaded')
 
-;
-(function() {
+function roamMonkey_include(url) {
+    const type = url.split('.').pop() // extension "js" or "css" // or go by ajax header
+
+    if (type == "js") {
+
+        // remove duplicates
+        const els = Array.from(document.getElementsByTagName('script'))
+        els.filter(el => el.src == url).forEach(el => el.remove())
+
+        // add script
+        const el = document.createElement('script')
+        el.src = url
+        document.getElementsByTagName('head')[0].appendChild(el)
+
+    } else if (type == "css") {
+
+        // remove duplicates
+        const els = Array.from(document.getElementsByTagName('link'))
+        els.filter(el => el.href == url).forEach(el => el.remove())
+
+        // add css
+        const el = document.createElement('link')
+        el.href = url
+        el.rel = 'stylesheet'
+        el.type = 'text/css'
+        document.getElementsByTagName('head')[0].appendChild(el)
+
+    } else {
+        alert(`Unknown type: ${type}`)
+        // continue if error
+    }
+}
+
+roamMonkey_include("https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js")
+
+setTimeout(init, 100)
+
+function init() {
 
     // if(!window.jQuery)
-    roamMonkey_include("https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js")
     roamMonkey_include("https://cdn.jsdelivr.net/npm/vue/dist/vue.js")
     roamMonkey_include("https://cdnjs.cloudflare.com/ajax/libs/axios/0.20.0/axios.min.js")
 
@@ -24,7 +59,7 @@ console.log('RoamMonkey: loaded')
 </span>`)
 
     searchBar.after(roammonkey_button)
-    templating_button.before(divider)
+    roammonkey_button.before(divider)
 
     var roammonkey = new Vue({
         el: '#roammonkey-app',
@@ -79,36 +114,5 @@ console.log('RoamMonkey: loaded')
         }
     })
 
-    function roamMonkey_include(url) {
-        const type = url.split('.').pop() // extension "js" or "css" // or go by ajax header
 
-        if (type == "js") {
-
-            // remove duplicates
-            const els = Array.from(document.getElementsByTagName('script'))
-            els.filter(el => el.src == url).forEach(el => el.remove())
-
-            // add script
-            const el = document.createElement('script')
-            el.src = url
-            document.getElementsByTagName('head')[0].appendChild(el)
-
-        } else if (type == "css") {
-
-            // remove duplicates
-            const els = Array.from(document.getElementsByTagName('link'))
-            els.filter(el => el.href == url).forEach(el => el.remove())
-
-            // add css
-            const el = document.createElement('link')
-            el.href = url
-            el.rel = 'stylesheet'
-            el.type = 'text/css'
-            document.getElementsByTagName('head')[0].appendChild(el)
-
-        } else {
-            alert(`Unknown type: ${type}`)
-            // continue if error
-        }
-    }
-})()
+}
