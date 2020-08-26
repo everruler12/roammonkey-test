cb8de48c6db52ca1125665f931b80af593b7cc28
everruler12 <erik.newhard@gmail.com>
2020-08-26 08:10:40 -0600

    Update roammonkey.js

--- roammonkey.js
+++ roammonkey.js
@@ -1,9 +1,7 @@
 console.log('RoamMonkey: loaded')
 
-
-
 // If a module is evaluated once, then imported again, it's second evaluation is skipped and the resolved already exports are used.
-// import "https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"
+import "https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"
 // import "https://cdn.jsdelivr.net/npm/vue/dist/vue.js"
 
 roamMonkey_init()
@@ -29,6 +27,7 @@ async function $roamMonkey_appendFile(url, attr) {
     let urlAttr // attribute that contains url: 'src' for <script> and 'href' for <link>
 
     if (ext == "js") {
+        // try importing as module first
         const mess = await importModule(url)
         console.error(`RoamMonkey: importModule: ${url}\n${mess}`)
         if (mess === true) return
@@ -71,51 +70,51 @@ async function $roamMonkey_appendFile(url, attr) {
     }
 }
 
-function roamMonkey_appendFile(url, attr) {
-    // return new Promise(resolve => {
-    attr = typeof attr == 'object' && !Array.isArray(attr) ? attr : {} // attr is an optional object containing attributes for <script> and <link>
+// function roamMonkey_appendFile(url, attr) {
+//     // return new Promise(resolve => {
+//     attr = typeof attr == 'object' && !Array.isArray(attr) ? attr : {} // attr is an optional object containing attributes for <script> and <link>
 
-    const ext = url.split('.').pop() // extension "js" or "css"
+//     const ext = url.split('.').pop() // extension "js" or "css"
 
-    let tag // html tag <script> or <link>
-    let urlAttr // attribute that contains url: 'src' for <script> and 'href' for <link>
+//     let tag // html tag <script> or <link>
+//     let urlAttr // attribute that contains url: 'src' for <script> and 'href' for <link>
 
-    if (ext == "js") {
-        tag = 'script'
-        urlAttr = 'src'
-    } else if (ext == "css") {
-        tag = 'link'
-        urlAttr = 'href'
-        attr.rel = 'stylesheet'
-        attr.type = 'text/css'
-    } else {
-        alert(`Unhandled file extension: ${ext}`)
-        console.log(`The file at ${url} does not have '.js' or '.css' extension.`)
-        return
-    }
+//     if (ext == "js") {
+//         tag = 'script'
+//         urlAttr = 'src'
+//     } else if (ext == "css") {
+//         tag = 'link'
+//         urlAttr = 'href'
+//         attr.rel = 'stylesheet'
+//         attr.type = 'text/css'
+//     } else {
+//         alert(`Unhandled file extension: ${ext}`)
+//         console.log(`The file at ${url} does not have '.js' or '.css' extension.`)
+//         return
+//     }
 
-    // stop if file already exists
-    const els = Array.from(document.getElementsByTagName(tag))
-    const duplicates = els.filter(el => el[urlAttr] == url)
-    if (duplicates.length > 0) {
-        console.log(`RoamMonkey: ${url} already exists.`)
-        return
-    }
+//     // stop if file already exists
+//     const els = Array.from(document.getElementsByTagName(tag))
+//     const duplicates = els.filter(el => el[urlAttr] == url)
+//     if (duplicates.length > 0) {
+//         console.log(`RoamMonkey: ${url} already exists.`)
+//         return
+//     }
 
-    // add file
-    attr[urlAttr] = url
-    // attr.onload = resolve('script loaded')
-    const el = document.createElement(tag)
-    Object.assign(el, attr)
-    document.head.appendChild(el)
-    // })
+//     // add file
+//     attr[urlAttr] = url
+//     // attr.onload = resolve('script loaded')
+//     const el = document.createElement(tag)
+//     Object.assign(el, attr)
+//     document.head.appendChild(el)
+//     // })
 
-}
+// }
 
 
 async function roamMonkey_init() {
-    roamMonkey_appendFile("https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js")
-    await roamMonkey_wait(() => window.jQuery)
+    // roamMonkey_appendFile("https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js")
+    // await roamMonkey_wait(() => window.jQuery)
     await $roamMonkey_appendFile("https://cdn.jsdelivr.net/npm/vue/dist/vue.js")
 
 
