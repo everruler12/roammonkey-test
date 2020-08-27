f9fdb904b46f120c1fe75791685db5da9799aa06
everruler12 <erik.newhard@gmail.com>
2020-08-26 20:30:25 -0600

    roam42-main.js

---
+++ scripts/roam42-main.js
@@ -0,0 +1,31 @@
+setTimeout(() => {
+
+    // Dont display in iframe
+    if (window === window.parent) {
+        displayStartup(6000)
+    }
+
+    setTimeout(() => {
+        if (device.mobile() == false) {
+            //these tools don't work well on mobile device
+            roamMonkey_appendFile('https://roam42.glitch.me/ext/roam-live-preview.js')
+            roamMonkey_appendFile('https://roam42.glitch.me/ext/dailynotespopup.js')
+        }
+
+        loadKeyEvents()
+
+        try {
+            loadTypeAhead()
+        } catch (e) {}
+
+        try {
+            jumpToDateComponent.initialize()
+        } catch (e) {}
+
+        try {
+            rmQuickRefenceSystem.initialize()
+        } catch (e) {}
+
+    }, 2000)
+
+}, 5000);
