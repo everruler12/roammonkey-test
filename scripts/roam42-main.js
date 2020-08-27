ef55ea2846e17127da6db1651b10e40cc7f5c0c4
everruler12 <erik.newhard@gmail.com>
2020-08-26 20:40:58 -0600

    Update roam42-main.js

--- scripts/roam42-main.js
+++ scripts/roam42-main.js
@@ -8,7 +8,7 @@ setTimeout(() => {
     setTimeout(() => {
         if (device.mobile() == false) {
             //these tools don't work well on mobile device
-            roamMonkey_appendFile('https://roam42.glitch.me/ext/roam-live-preview.js')
+            // roamMonkey_appendFile('https://roam42.glitch.me/ext/roam-live-preview.js')
             // roamMonkey_appendFile('https://roam42.glitch.me/ext/dailynotespopup.js')
         }
 
