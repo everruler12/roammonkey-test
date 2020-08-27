e06a9d82952a6c32841082b5aa24eae607737d60
everruler12 <erik.newhard@gmail.com>
2020-08-26 20:48:33 -0600

    fix displayHelp

--- scripts/roam42-main.js
+++ scripts/roam42-main.js
@@ -1,3 +1,29 @@
+// from https://roam42.glitch.me/message-s.js
+
+const displayStartup = (delayTime) => {
+    iziToast.show({
+        message: `
+      <b>Roam42 Starting . . .</b>
+      <p></p>
+      <table>
+        <tr><td>Altâ€“Shiftâ€“H </td><td>&nbsp</td><td>Roam42 Help</td></tr>
+        <tr><td>Ctrlâ€“Shiftâ€“H</td><td>&nbsp</td><td>Quick Reference</td></tr>
+      </table>
+      <p></p>
+      <div style='font-size:7pt'>marvin.2020-08-23</div>
+    `.trim(),
+        theme: 'dark',
+        progressBar: true,
+        animateInside: true,
+        close: false,
+        timeout: delayTime,
+        closeOnClick: true,
+        displayMode: 2
+    });
+}
+
+//from https://roam42.glitch.me/main.js
+
 setTimeout(() => {
 
     // Dont display in iframe
