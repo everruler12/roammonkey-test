61095d01429e32d9dbdf7c528a72dff4dae9cee1
everruler12 <erik.newhard@gmail.com>
2020-09-03 10:35:04 -0600

    Update roammonkey.js

--- roammonkey.js
+++ roammonkey.js
@@ -164,19 +164,50 @@ async function roamMonkey_initVue(packages) {
 </span>`)
 
     const panel = $( /* html */ `
-<div class="bp3-overlay bp3-overlay-open bp3-overlay-inline" v-show="showPanel">
-    <div class="bp3-transition-container bp3-popover-enter-done" style="position: absolute; will-change: transform; top: 0px; left: 0px; transform: translate3d(-260px, 0px, 0px);">
-        <div class="bp3-popover bp3-minimal rm-fill" style="transform-origin: center top;">
-            <div class="bp3-popover-content">
-                <div class="bp3-tabs">
-                    <ul class="bp3-tab-list" role="tablist">
-                        <li class="bp3-tab" role="tab" aria-selected="true">Selected tab</li>
-                        <li class="bp3-tab" role="tab">Another tab</li>
-                    </ul>
-                    <div class="bp3-tab-panel" role="tabpanel">Selected panel</div>
-                    <div class="bp3-tab-panel" role="tabpanel" aria-hidden="true">Another panel</div>
-                </div>
+<div class="bp3-overlay bp3-overlay-open bp3-overlay-scroll-container">
+    <div class="bp3-overlay-backdrop bp3-overlay-enter-done" tabindex="0"></div>
+    <div class="bp3-card bp3-elevation-4 docs-overlay-example-transition bp3-overlay-content bp3-overlay-enter-done" tabindex="0">
+        <h3 class="bp3-heading">I'm an Overlay!</h3>
+        <p>This is a simple container with some inline styles to position it on the screen. Its CSS transitions are customized for this example only to demonstrate how easily custom transitions can be implemented.</p>
+
+        <div class="bp3-tabs">
+            <ul class="bp3-tab-list" role="tablist">
+                <li class="bp3-tab" role="tab" aria-selected="true">Selected tab</li>
+                <li class="bp3-tab" role="tab">Another tab</li>
+            </ul>
+            <div class="bp3-tab-panel" role="tabpanel">Selected panel
+                <label class="bp3-control bp3-switch .modifier">
+                    <input type="checkbox" :modifier />
+                    <span class="bp3-control-indicator"></span>
+                    Switch
+                </label>
             </div>
+            <div class="bp3-tab-panel" role="tabpanel" aria-hidden="true">Another panel</div>
+        </div>
+
+        <p>Click the "Focus button" below to transfer focus to the "Show overlay" trigger button outside of this overlay. If persistent focus is enabled, focus will be constrained to the overlay. Use the <code class="bp3-code">tab</code> key to move to the next focusable element to illustrate this effect.</p>
+        <p>Click the "Make me scroll" button below to make this overlay's content really tall, which will make the overlay's container (but not the page) scrollable</p><br>
+        <div class="bp3-dialog-footer-actions">
+            <button type="button" class="bp3-button bp3-intent-danger">
+                <span class="bp3-button-text">Close</span>
+            </button>
+
+            <button type="button" class="bp3-button">
+                <span class="bp3-button-text">Focus button</span>
+            </button>
+
+            <button type="button" class="bp3-button">
+                <span icon="double-chevron-down" class="bp3-icon bp3-icon-double-chevron-down"><svg data-icon="double-chevron-down" width="16" height="16" viewBox="0 0 16 16">
+                        <desc>double-chevron-down</desc>
+                        <path d="M7.29 8.71c.18.18.43.29.71.29s.53-.11.71-.29l4-4a1.003 1.003 0 00-1.42-1.42L8 6.59l-3.29-3.3a1.003 1.003 0 00-1.42 1.42l4 4zM12 8c-.28 0-.53.11-.71.29L8 11.59l-3.29-3.3a1.003 1.003 0 00-1.42 1.42l4 4c.18.18.43.29.71.29s.53-.11.71-.29l4-4A1.003 1.003 0 0012 8z" fill-rule="evenodd"></path>
+                    </svg>
+                </span>
+                <span class="bp3-button-text">Make me scroll</span><span icon="double-chevron-down" class="bp3-icon bp3-icon-double-chevron-down"><svg data-icon="double-chevron-down" width="16" height="16" viewBox="0 0 16 16">
+                        <desc>double-chevron-down</desc>
+                        <path d="M7.29 8.71c.18.18.43.29.71.29s.53-.11.71-.29l4-4a1.003 1.003 0 00-1.42-1.42L8 6.59l-3.29-3.3a1.003 1.003 0 00-1.42 1.42l4 4zM12 8c-.28 0-.53.11-.71.29L8 11.59l-3.29-3.3a1.003 1.003 0 00-1.42 1.42l4 4c.18.18.43.29.71.29s.53-.11.71-.29l4-4A1.003 1.003 0 0012 8z" fill-rule="evenodd"></path>
+                    </svg>
+                </span>
+            </button>
         </div>
     </div>
 </div>`)
