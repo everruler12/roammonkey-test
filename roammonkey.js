6216f380b9c529f5b4aa95c1af0c06621775d660
everruler12 <erik.newhard@gmail.com>
2020-09-18 09:54:31 -0600

    Update roammonkey.js

--- roammonkey.js
+++ roammonkey.js
@@ -8,7 +8,7 @@ if (window.roamMonkey) window.roamMonkey.$destroy() // what about when other roa
 
 window.roamMonkey = new Vue({
     data: {
-        appId: 'roamMonkey-app',
+        vueAppId: 'roamMonkey-app',
         package_registry_link: 'https://roammonkey-test.vercel.app/roam_packages/roam_package_registry.json',
         // package_registry: [
         //     'https://roammonkey-test.vercel.app/roam_packages(ViktorTabori).json',
@@ -26,103 +26,20 @@ window.roamMonkey = new Vue({
         // }
     },
 
-    methods: {
-        appendFile(url, attr) {
-            attr = typeof attr == 'object' && !Array.isArray(attr) ? attr : {} // attr is an optional object containing attributes for <script> and <link>
-
-            const ext = url.split('.').pop() // extension "js" or "css"
-
-            let tag // html tag <script> or <link>
-            let urlAttr // attribute that contains url: 'src' for <script> and 'href' for <link>
-
-            if (ext == "js") {
-                tag = 'script'
-                urlAttr = 'src'
-            } else if (ext == "css") {
-                tag = 'link'
-                urlAttr = 'href'
-                attr.rel = 'stylesheet'
-                attr.type = 'text/css'
-            } else {
-                alert(`Unhandled file extension: ${ext}`)
-                console.log(`The file at ${url} does not have '.js' or '.css' extension.`)
-                return
-            }
-
-            // stop if file already exists
-            const els = Array.from(document.getElementsByTagName(tag))
-            const duplicates = els.filter(el => el[urlAttr] == url)
-            if (duplicates.length > 0) {
-                console.log(`RoamMonkey: already exists, not appended ${url}`)
-                return
-            }
-
-            // add file
-            attr[urlAttr] = url
-            const el = document.createElement(tag)
-            Object.assign(el, attr)
-            document.head.appendChild(el)
-            console.log(`RoamMonkey: appended ${url}`)
-        },
-
-        refresh() {
-            function refreshAfterSync() {
-                const syncing = document.getElementsByClassName('rm-saving-remote').length
-                if (syncing) setTimeout(refreshAfterSync, 50)
-                else location.reload(true) // refresh page
-            }
-            setTimeout(refreshAfterSync, 100)
-        }
-
-    },
-    mounted() {
-        console.log('RoamMonkey: mounted')
-    },
-
     destroyed() {
-        const appEl = $(`#${roamMonkey.appId}`)
+        // remove Vue app el
+        const appEl = $('#' + this.vueAppId)
         appEl.prev('div').remove() // divider
         appEl.remove()
     },
 
     async created() {
-        async function loadPackage(url) {
-            let res = await fetch(url) // fetch is built in on most popular browsers
-            let json = await res.json()
-            return json.packages //.forEach(pack => packages.push(pack))
-        }
-
-        function parsePackage(pack) {
-            // check enabled
-
-            if (pack.dependencies) {
-                if (typeof pack.dependencies == "string") roamMonkey.appendFile(pack.dependencies)
-                else if (Array.isArray(pack.dependencies)) pack.dependencies.map(roamMonkey.appendFile)
-            }
-
-            if (pack.source) {
-                if (typeof pack.source == "string") roamMonkey.appendFile(pack.source)
-                else if (Array.isArray(pack.source)) pack.source.map(roamMonkey.appendFile)
-            }
-
-        }
-
-        let packages = await loadPackage(roamMonkey.package_registry_link)
-        // let packages = await Promise.all(roamMonkey.package_registry.map(loadPackage))
-        console.log('packages', packages)
-        packages = packages.reduce((a, b) => a.concat(b), []) // flatten array
-        roamMonkey.package_registry = packages
-
-        // load localStorage, go through roamMonkey.packages and overwrite each setting property if it exists in ls
-        packages.forEach(parsePackage) // only if enabled
-
-
-        // mount button
+        // mount Vue app el
         const searchBar = $('.rm-find-or-create-wrapper').eq(0)
         const divider = $( /* html */ `<div style="flex: 0 0 4px"></div>`)
 
-        const roamMonkey_button = $( /* html */ `
-<span id="${roamMonkey.appId}" class="bp3-popover-wrapper">
+        const vueAppEl = $( /* html */ `
+<span id="${this.vueAppId}" class="bp3-popover-wrapper">
 <span class="bp3-popover-target">
     <span class="bp3-popover-target">
         <button class="bp3-button bp3-minimal bp3-icon-comparison bp3-small" tabindex="0" title="RoamMonkey" @click="showPanel=!showPanel"></button>
@@ -180,11 +97,98 @@ window.roamMonkey = new Vue({
 </div>
 </div>`)
 
-        searchBar.after(roamMonkey_button)
-        roamMonkey_button.append(panel)
-        roamMonkey_button.before(divider)
-        this.$mount(`#${roamMonkey.appId}`)
-    }
+        searchBar.after(vueAppEl)
+        vueAppEl.append(panel)
+        vueAppEl.before(divider)
+        this.$mount('#' + this.vueAppId)
+    },
+
+    mounted() {
+        console.log('RoamMonkey: mounted')
+
+        async function loadPackage(url) {
+            let res = await fetch(url) // fetch is built in on most popular browsers
+            let json = await res.json()
+            return json.packages //.forEach(pack => packages.push(pack))
+        }
+
+        function parsePackage(pack) {
+            // check enabled
+
+            if (pack.dependencies) {
+                if (typeof pack.dependencies == "string") this.appendFile(pack.dependencies)
+                else if (Array.isArray(pack.dependencies)) pack.dependencies.map(this.appendFile)
+            }
+
+            if (pack.source) {
+                if (typeof pack.source == "string") this.appendFile(pack.source)
+                else if (Array.isArray(pack.source)) pack.source.map(this.appendFile)
+            }
+
+        }
+
+        let packages = await loadPackage(this.package_registry_link)
+        // let packages = await Promise.all(this.package_registry.map(loadPackage))
+        console.log('packages', packages)
+        packages = packages.reduce((a, b) => a.concat(b), []) // flatten array
+        this.package_registry = packages
+
+        // load localStorage, go through this.packages and overwrite each setting property if it exists in ls
+        packages.forEach(parsePackage) // only if enabled
+    },
+
+    methods: {
+        appendFile(url, attr) {
+            attr = typeof attr == 'object' && !Array.isArray(attr) ? attr : {} // attr is an optional object containing attributes for <script> and <link>
+
+            const ext = url.split('.').pop() // extension "js" or "css"
+
+            let tag // html tag <script> or <link>
+            let urlAttr // attribute that contains url: 'src' for <script> and 'href' for <link>
+
+            if (ext == "js") {
+                tag = 'script'
+                urlAttr = 'src'
+            } else if (ext == "css") {
+                tag = 'link'
+                urlAttr = 'href'
+                attr.rel = 'stylesheet'
+                attr.type = 'text/css'
+            } else {
+                alert(`Unhandled file extension: ${ext}`)
+                console.log(`The file at ${url} does not have '.js' or '.css' extension.`)
+                return
+            }
+
+            // stop if file already exists
+            const els = Array.from(document.getElementsByTagName(tag))
+            const duplicates = els.filter(el => el[urlAttr] == url)
+            if (duplicates.length > 0) {
+                console.log(`RoamMonkey: already exists, not appended ${url}`)
+                return
+            }
+
+            // add file
+            attr[urlAttr] = url
+            const el = document.createElement(tag)
+            Object.assign(el, attr)
+            document.head.appendChild(el)
+            console.log(`RoamMonkey: appended ${url}`)
+        },
+
+        refresh() {
+            function refreshAfterSync() {
+                const syncing = document.getElementsByClassName('rm-saving-remote').length
+                if (syncing) setTimeout(refreshAfterSync, 50)
+                else location.reload(true) // refresh page
+            }
+            setTimeout(refreshAfterSync, 100)
+        }
+
+    },
+
+
+
 })
 
 // on panel, have one tab for pacakage registry, which lists all packages, with the option to add to ac, then on list tab, each have toggle, option to delete
