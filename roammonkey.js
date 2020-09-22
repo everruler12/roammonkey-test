3274bd5ff39c8558c361412751abd6a1de6d143d
everruler12 <erik.newhard@gmail.com>
2020-09-22 10:27:10 -0600

    Update roammonkey.js

--- roammonkey.js
+++ roammonkey.js
@@ -1,138 +1,138 @@
 // Using imports to prevent duplicates and to wait for jQuery to initialize before continuing
 import "https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"
 import "https://cdn.jsdelivr.net/npm/vue/dist/vue.js"
-alert('hi')
-    (function() {
-        // Checks to stop execution of whole script
-        if ($('script[src$="/roammonkey.js"]').length > 1) { // RoamMonkey is duplicated
-            // Every time a roam/js block is started or stopped, all other roam/js blocks are reloaded.
-            // This detects whether RoamMonkey is already loaded, and will prevent further execution if duplicated.
-            console.log('RoamMonkey: already loaded. Please refresh the page to load again.')
-            return
-        } else if (!!navigator.userAgent.match('Headless')) { // stop if roam-to-git
-            // This will prevent loading other scripts if Roam is being fetched for an automatic backup
-            console.log('RoamMonkey: Headless user agent detected. Stopped script.')
-            return
-        } else {
-            console.log('RoamMonkey: loaded')
+alert('hi');
+(function() {
+    // Checks to stop execution of whole script
+    if ($('script[src$="/roammonkey.js"]').length > 1) { // RoamMonkey is duplicated
+        // Every time a roam/js block is started or stopped, all other roam/js blocks are reloaded.
+        // This detects whether RoamMonkey is already loaded, and will prevent further execution if duplicated.
+        console.log('RoamMonkey: already loaded. Please refresh the page to load again.')
+        return
+    } else if (!!navigator.userAgent.match('Headless')) { // stop if roam-to-git
+        // This will prevent loading other scripts if Roam is being fetched for an automatic backup
+        console.log('RoamMonkey: Headless user agent detected. Stopped script.')
+        return
+    } else {
+        console.log('RoamMonkey: loaded')
+    }
+
+
+
+    window.roamMonkey = (function() { // shared functions
+
+        function appendFile(url, attr) {
+            // url must end with .js or .css
+            // attr is an optional object containing attributes for <script> and <link>
+
+            attr = typeof attr == 'object' && !Array.isArray(attr) ? attr : {}
+
+            const ext = url.split('.').pop() // extension "js" or "css"
+
+            let tag // html tag <script> or <link>
+            let urlAttr // attribute that will contain url: 'src' for <script> and 'href' for <link>
+
+            if (ext == "js") {
+                tag = 'script'
+                urlAttr = 'src'
+            } else if (ext == "css") {
+                tag = 'link'
+                urlAttr = 'href'
+                attr.rel = 'stylesheet'
+                attr.type = 'text/css'
+            } else {
+                alert(`Unhandled file extension: ${ext}`)
+                console.log(`The file at ${url} does not have '.js' or '.css' extension.`)
+                return
+            }
+
+            // stop if file already exists
+            const els = Array.from(document.getElementsByTagName(tag))
+            const duplicate = !!els.find(el => el[urlAttr] == url)
+            if (duplicate) {
+                console.log(`RoamMonkey: already exists, not appended ${url}`)
+                return
+            }
+
+            // add file
+            attr[urlAttr] = url
+            const el = document.createElement(tag)
+            Object.assign(el, attr)
+            document.head.appendChild(el)
+            console.log(`RoamMonkey: appended ${url}`)
         }
 
-
-
-        window.roamMonkey = (function() { // shared functions
-
-            function appendFile(url, attr) {
-                // url must end with .js or .css
-                // attr is an optional object containing attributes for <script> and <link>
-
-                attr = typeof attr == 'object' && !Array.isArray(attr) ? attr : {}
-
-                const ext = url.split('.').pop() // extension "js" or "css"
-
-                let tag // html tag <script> or <link>
-                let urlAttr // attribute that will contain url: 'src' for <script> and 'href' for <link>
-
-                if (ext == "js") {
-                    tag = 'script'
-                    urlAttr = 'src'
-                } else if (ext == "css") {
-                    tag = 'link'
-                    urlAttr = 'href'
-                    attr.rel = 'stylesheet'
-                    attr.type = 'text/css'
-                } else {
-                    alert(`Unhandled file extension: ${ext}`)
-                    console.log(`The file at ${url} does not have '.js' or '.css' extension.`)
-                    return
-                }
-
-                // stop if file already exists
-                const els = Array.from(document.getElementsByTagName(tag))
-                const duplicate = !!els.find(el => el[urlAttr] == url)
-                if (duplicate) {
-                    console.log(`RoamMonkey: already exists, not appended ${url}`)
-                    return
-                }
-
-                // add file
-                attr[urlAttr] = url
-                const el = document.createElement(tag)
-                Object.assign(el, attr)
-                document.head.appendChild(el)
-                console.log(`RoamMonkey: appended ${url}`)
+        function refresh() {
+            function refreshAfterSync() {
+                const isSyncing = document.getElementsByClassName('rm-saving-remote').length
+                if (isSyncing) setTimeout(refreshAfterSync, 50)
+                else location.reload() // refresh page
             }
+            setTimeout(refreshAfterSync, 100)
+        }
 
-            function refresh() {
-                function refreshAfterSync() {
-                    const isSyncing = document.getElementsByClassName('rm-saving-remote').length
-                    if (isSyncing) setTimeout(refreshAfterSync, 50)
-                    else location.reload() // refresh page
-                }
-                setTimeout(refreshAfterSync, 100)
-            }
-
-            return {
-                appendFile,
-                refresh
-            }
-        })()
+        return {
+            appendFile,
+            refresh
+        }
+    })()
 
 
 
-        new Vue({
-            data: {
-                VUE_APP_NAME: 'roamMonkeyVue',
-                titleOfSettingsPage: 'RoamMonkey/settings',
-                package_registry_link: 'https://roammonkey-test.vercel.app/roam_packages/roam_packages(ViktorTabori).json',
-                // package_registry_link: 'https://roammonkey-test.vercel.app/roam_packages/roam_package_registry.json',
-                // package_registry: [
-                //     'https://roammonkey-test.vercel.app/roam_packages(ViktorTabori).json',
-                //     'https://roammonkey-test.vercel.app/roam_packages(roamhacker).json'
-                // ],
-                package_registry: [],
-                showPanel: false,
-                panel_tab: "Scripts"
+    new Vue({
+        data: {
+            VUE_APP_NAME: 'roamMonkeyVue',
+            titleOfSettingsPage: 'RoamMonkey/settings',
+            package_registry_link: 'https://roammonkey-test.vercel.app/roam_packages/roam_packages(ViktorTabori).json',
+            // package_registry_link: 'https://roammonkey-test.vercel.app/roam_packages/roam_package_registry.json',
+            // package_registry: [
+            //     'https://roammonkey-test.vercel.app/roam_packages(ViktorTabori).json',
+            //     'https://roammonkey-test.vercel.app/roam_packages(roamhacker).json'
+            // ],
+            package_registry: [],
+            showPanel: false,
+            panel_tab: "Scripts"
 
+        },
+
+        computed: {
+            VUE_APP_ID() {
+                return this.VUE_APP_NAME + '-app'
             },
 
-            computed: {
-                VUE_APP_ID() {
-                    return this.VUE_APP_NAME + '-app'
-                },
 
+        },
 
-            },
+        destroyed() {
+            console.log(this.VUE_APP_NAME + ': destroyed')
+            // remove mounted Vue app el
+            const appEl = $('#' + this.VUE_APP_ID)
+            appEl.prev('div').remove() // divider
+            appEl.remove()
+        },
 
-            destroyed() {
-                console.log(this.VUE_APP_NAME + ': destroyed')
-                // remove mounted Vue app el
-                const appEl = $('#' + this.VUE_APP_ID)
-                appEl.prev('div').remove() // divider
-                appEl.remove()
-            },
+        created() {
+            // console.log(this.VUE_APP_NAME + ': created')
+            const appName = this.VUE_APP_NAME
+            if (!!window[appName]) // already exists
+                window[appName].$destroy() // what about when other roam/js loaded? Keep refresh in this roam/js?
 
-            created() {
-                // console.log(this.VUE_APP_NAME + ': created')
-                const appName = this.VUE_APP_NAME
-                if (!!window[appName]) // already exists
-                    window[appName].$destroy() // what about when other roam/js loaded? Keep refresh in this roam/js?
+            window[appName] = this // to test in DevTools
+            this.mountVueApp()
+        },
 
-                window[appName] = this // to test in DevTools
-                this.mountVueApp()
-            },
+        mounted() {
+            console.log(this.VUE_APP_NAME + ': mounted')
+            this.loadPackages()
+        },
 
-            mounted() {
-                console.log(this.VUE_APP_NAME + ': mounted')
-                this.loadPackages()
-            },
+        methods: {
+            mountVueApp() {
+                const appId = this.VUE_APP_ID
+                const searchBar = $('.rm-find-or-create-wrapper').eq(0)
+                const divider = $( /* html */ `<div style="flex: 0 0 4px"></div>`)
 
-            methods: {
-                mountVueApp() {
-                    const appId = this.VUE_APP_ID
-                    const searchBar = $('.rm-find-or-create-wrapper').eq(0)
-                    const divider = $( /* html */ `<div style="flex: 0 0 4px"></div>`)
-
-                    const appEl = $( /* html */ `
+                const appEl = $( /* html */ `
 <span id="${appId}" class="bp3-popover-wrapper">
 <span class="bp3-popover-target">
 <span class="bp3-popover-target">
@@ -141,7 +141,7 @@ alert('hi')
 </span>
 </span>`)
 
-                    const panel = $( /* html */ `
+                const panel = $( /* html */ `
 <div class="bp3-overlay bp3-overlay-open bp3-overlay-scroll-container" v-show="showPanel" style="margin: 250px;">
 <div class="bp3-overlay-backdrop bp3-overlay-enter-done" tabindex="0"></div>
 <div class="bp3-card bp3-elevation-4 bp3-overlay-content bp3-overlay-enter-done" tabindex="0" style="width: 100%;">
@@ -191,109 +191,109 @@ alert('hi')
 </div>
 </div>`)
 
-                    searchBar.after(appEl)
-                    appEl.append(panel)
-                    appEl.before(divider)
-                    this.$mount('#' + appId)
-                },
+                searchBar.after(appEl)
+                appEl.append(panel)
+                appEl.before(divider)
+                this.$mount('#' + appId)
+            },
 
-                async loadPackages() {
-                    async function loadPackage(url) {
-                        let res = await fetch(url) // fetch is built in on most popular browsers
-                        let json = await res.json()
-                        return json.packages //.forEach(pack => packages.push(pack))
+            async loadPackages() {
+                async function loadPackage(url) {
+                    let res = await fetch(url) // fetch is built in on most popular browsers
+                    let json = await res.json()
+                    return json.packages //.forEach(pack => packages.push(pack))
+                }
+
+                const parsePackage = (pack) => {
+                    // check enabled
+
+                    if (pack.dependencies) {
+                        if (typeof pack.dependencies == "string") roamMonkey.appendFile(pack.dependencies)
+                        else if (Array.isArray(pack.dependencies)) pack.dependencies.map(roamMonkey.appendFile)
                     }
 
-                    const parsePackage = (pack) => {
-                        // check enabled
-
-                        if (pack.dependencies) {
-                            if (typeof pack.dependencies == "string") roamMonkey.appendFile(pack.dependencies)
-                            else if (Array.isArray(pack.dependencies)) pack.dependencies.map(roamMonkey.appendFile)
-                        }
-
-                        if (pack.source) {
-                            if (typeof pack.source == "string") roamMonkey.appendFile(pack.source)
-                            else if (Array.isArray(pack.source)) pack.source.map(roamMonkey.appendFile)
-                        }
-
+                    if (pack.source) {
+                        if (typeof pack.source == "string") roamMonkey.appendFile(pack.source)
+                        else if (Array.isArray(pack.source)) pack.source.map(roamMonkey.appendFile)
                     }
 
-                    let packages = await loadPackage(this.package_registry_link)
-                    // let packages = await Promise.all(this.package_registry.map(loadPackage))
-                    // packages = packages.reduce((a, b) => a.concat(b), []) // flatten array
-                    console.log('packages', packages)
-                    this.package_registry = packages
+                }
 
-                    // load localStorage, go through this.packages and overwrite each setting property if it exists in ls
-                    packages.forEach(parsePackage) // only if enabled
-                },
+                let packages = await loadPackage(this.package_registry_link)
+                // let packages = await Promise.all(this.package_registry.map(loadPackage))
+                // packages = packages.reduce((a, b) => a.concat(b), []) // flatten array
+                console.log('packages', packages)
+                this.package_registry = packages
 
-                save() {
+                // load localStorage, go through this.packages and overwrite each setting property if it exists in ls
+                packages.forEach(parsePackage) // only if enabled
+            },
+
+            save() {
 
 
-                    roamMonkey.refresh()
-                },
+                roamMonkey.refresh()
+            },
 
-                loadSettings() {
-                    function returnIdOfPageTitle(title) {
-                        const nodeId = window.roamAlphaAPI.q("[:find ?e :in $ ?a :where [?e :node/title ?a]]", title)
+            loadSettings() {
+                function returnIdOfPageTitle(title) {
+                    const nodeId = window.roamAlphaAPI.q("[:find ?e :in $ ?a :where [?e :node/title ?a]]", title)
 
-                        if (nodeId.length) {
-                            return nodeId[0][0]
-                        } else {
-                            console.log(this.VUE_APP_NAME + `: Error! [[${title}]] not found`)
-                            return false
-                        }
-                    }
-
-                    const titleId = returnIdOfPageTitle(this.titleOfSettingsPage)
-
-                    if (!titleId) {
-                        alert('Error! No settings page found')
-                        return
-                    }
-
-                    const titleNode = window.roamAlphaAPI.pull("[*]", titleId)
-
-                    if (!titleNode) {
-                        alert('Error! No settings page found')
-                        return
-                    }
-
-                    // for backup purposes, go to settings page
-                    // const blockUid = titleNode[":block/uid"]
-                    // settings_page = 'https://roamresearch.com/#/app/everruler/page/' + blockUid
-                    // window.location = settings_page
-
-                    const titleChildren = titleNode[":block/children"]
-
-                    if (titleChildren.length > 1) {
-                        alert(`Error! There should only be one block on RoamMonkey/settings, but there are ${titleChildren.length} blocks`)
-                        return
-                    }
-
-                    const firstChildId = titleChildren[0][":db/id"]
-                    const firstChildNode = window.roamAlphaAPI.pull("[*]", firstChildId)
-                    const firstChildContent = firstChildNode[":block/string"]
-                    // console.log(blockString)
-                    const settings_str = firstChildContent.replace(/^\u0060\u0060\u0060javascript/, '').replace(/\u0060\u0060\u0060$/, '') // \u0060 is backtick: `
-
-                    try {
-                        return JSON.parse(settings_str)
-                    } catch (err) {
-                        console.log(this.VUE_APP_NAME + `: ${settings_str} is not valid json`)
-                        alert('Error! Content of first block in RoamMonkey/settings is not valid json')
+                    if (nodeId.length) {
+                        return nodeId[0][0]
+                    } else {
+                        console.log(this.VUE_APP_NAME + `: Error! [[${title}]] not found`)
                         return false
                     }
                 }
 
-            },
+                const titleId = returnIdOfPageTitle(this.titleOfSettingsPage)
+
+                if (!titleId) {
+                    alert('Error! No settings page found')
+                    return
+                }
+
+                const titleNode = window.roamAlphaAPI.pull("[*]", titleId)
+
+                if (!titleNode) {
+                    alert('Error! No settings page found')
+                    return
+                }
+
+                // for backup purposes, go to settings page
+                // const blockUid = titleNode[":block/uid"]
+                // settings_page = 'https://roamresearch.com/#/app/everruler/page/' + blockUid
+                // window.location = settings_page
+
+                const titleChildren = titleNode[":block/children"]
+
+                if (titleChildren.length > 1) {
+                    alert(`Error! There should only be one block on RoamMonkey/settings, but there are ${titleChildren.length} blocks`)
+                    return
+                }
+
+                const firstChildId = titleChildren[0][":db/id"]
+                const firstChildNode = window.roamAlphaAPI.pull("[*]", firstChildId)
+                const firstChildContent = firstChildNode[":block/string"]
+                // console.log(blockString)
+                const settings_str = firstChildContent.replace(/^\u0060\u0060\u0060javascript/, '').replace(/\u0060\u0060\u0060$/, '') // \u0060 is backtick: `
+
+                try {
+                    return JSON.parse(settings_str)
+                } catch (err) {
+                    console.log(this.VUE_APP_NAME + `: ${settings_str} is not valid json`)
+                    alert('Error! Content of first block in RoamMonkey/settings is not valid json')
+                    return false
+                }
+            }
+
+        },
 
 
 
-        })
-    })()
+    })
+})()
 
 
 
